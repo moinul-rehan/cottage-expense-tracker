98c38985aab143a42400d34be918f49e8ad46068
Moinul Islam Rehan <rehan.moinul.10@gmail.com>
2026-08-06 02:17:42 +0600

    Flutter: remove duplicate core-library-desugaring block from merge

--- Flutter App/android/app/build.gradle.kts
+++ Flutter App/android/app/build.gradle.kts
@@ -19,7 +19,6 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
         // flutter_local_notifications requires this (uses java.time APIs
@@ -59,8 +58,4 @@ dependencies {
 
 flutter {
     source = "../.."
-}
-
-dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
